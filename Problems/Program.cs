76803a3207d16813c196ddee487ba6fc95e114f5
Robin <gvgollie4@gmail.com>
2019-02-21 15:26:47 +0200

    Unit tests added

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
-
+//using System.String;
 namespace Problems
 {
-    class Program
+    public class Program
     {
+       private string Prefix(string input)
+        {
+            int length=input.Length; //get string length
+            string[] wordcount = input.Split(' '); //Seperate by words into array
+            int count = wordcount.Length; //Get the wordcount
+            string output = $"{length},{count}:{input}"; //Put it all together
+            return output;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
